ad9fcfe20ebaea9d796a8e4de245d078abf2104e
ngani <nganip1@gmail.com>
2021-07-19 16:51:17 -0400

    Initialise

--- train.py
+++ train.py
@@ -72,7 +72,7 @@ def mlflow_run(params, run_name="Tracking Experiment: TensorFlow - CNN "):
     # Use the model
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     depth = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     trees = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
